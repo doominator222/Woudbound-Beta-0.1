519c0fc07e9a98bae7f45c29b513c4cdee7e7f09
doominator222 <46983182+doominator222@users.noreply.github.com>
2019-11-29 18:40:32 -0500

    Add files via upload

---
+++ BoxPuzzle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPuzzle : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy"))
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        else if (collision.CompareTag("BoxButton"))
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("enemy"))
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
---
+++ DoorPuzzle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPuzzle : MonoBehaviour
+{
+    public static bool OnOff;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (OnOff == true)
+        {
+            OnOff = false;
+        }
+        else if (OnOff == false)
+        {
+            OnOff = true;
+        }
+    }
+    
+}
